0c7cefcb0f22613c1db9776a75ac15b67b4412d1
Jayaprakash N <n.jayaprakash@intel.com>
2024-06-14 20:44:39 +0530

    Added support for enabling the python368.inf file in AppPkg.dsc file

---
+++ .github/scripts/enable_pyuefi_apppkg.py
@@ -0,0 +1,31 @@
+'''Script to enable the build of python UEFI interpreter 
+   in AppPkg.dsc file
+'''
+import os
+
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+
+# path to the AppPkg.dsc file
+path_to_AppPkg_dsc = os.path.join(script_dir, '..', '..', 'AppPkg', 'AppPkg.dsc')
+print('Path to AppPkg dsc file : ', path_to_AppPkg_dsc)
+
+# Check if the file exists
+if not os.path.isfile(path_to_AppPkg_dsc):
+    print(f"The file {path_to_AppPkg_dsc} does not exist.")
+else:
+    # Read the content of the file
+    with open(path_to_AppPkg_dsc, 'r') as file:
+        lines = file.readlines()
+
+    # Uncomment the line containing "Python368.inf"
+    with open(path_to_AppPkg_dsc, 'w') as file:
+        for line in lines:
+            if 'Python368.inf' in line and line.strip().startswith('#'):
+                # Uncomment the line
+                file.write(line.lstrip('#'))
+            else:
+                file.write(line)
+
+    print(f"The file {path_to_AppPkg_dsc} has been updated.")
